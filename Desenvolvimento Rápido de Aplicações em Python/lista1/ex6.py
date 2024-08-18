c058d24170389effe15faceb5aac043b74d63c8e
Davi <oversouls11@gmail.com>
2024-08-18 20:34:44 -0300

    Arrumando exercício 6

--- Desenvolvimento Rápido de Aplicações em Python/lista1/ex6.py
+++ Desenvolvimento Rápido de Aplicações em Python/lista1/ex6.py
@@ -10,7 +10,10 @@ def count_search_letter(word: str, letter: str, count: int = 0) -> int:
         
     return count_search_letter(word[1:], letter, count)
 
-word: str = input("Digite uma palavra: ")
+word: str = input("Digite uma frase: ")
+word.replace(" ", "")
+word.lower() 
+# coloquei para ficar minúsculo, porém o próprio computador pela tabela ASCII entende minúsculo e maiusculo como diferentes
 letter: str = input("Digite uma letra para ver sua quantidade: ")
 
 print(count_search_letter(word, letter))
