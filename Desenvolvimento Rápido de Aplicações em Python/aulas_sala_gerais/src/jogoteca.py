aedfc62e86e2a337b3b880474d5bfd4ce987c208
Davi <oversouls11@gmail.com>
2024-09-16 18:20:20 -0300

    Continuação estudos

--- Desenvolvimento Rápido de Aplicações em Python/aulas_sala_gerais/src/jogoteca.py
+++ Desenvolvimento Rápido de Aplicações em Python/aulas_sala_gerais/src/jogoteca.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+class Jogo:
+    def __init__(self, nome: str, categoria: str, console: str):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 
 app = Flask(__name__)
 
-@app.route("/inicio")
-def ola():
-    lista: list[str] = ["God of war", "Super Mario", "GTA V"]
+jogo1: Jogo = Jogo(nome="God of War", categoria="Ação", console="PS4")
+jogo2: Jogo = Jogo("Skyrim", "RPG", "PS4")
+jogo3: Jogo = Jogo("Fifa 21", "Esportes", "PS4")
     
+lista: list[Jogo] = [jogo1, jogo2, jogo3]
+
+@app.route("/inicio")
+def ola() -> str:
+    
+    return render_template("lista.html", titulo="Jogos", jogos=lista)
+
+@app.route("/novo")
+def novo() -> str:
+    return render_template("novo.html", titulo="Novo Jogo")
+
+@app.route("/criar", methods=["POST"])
+def criar() -> str:
+    nome: str = request.form["nome"]
+    categoria: str = request.form["categoria"]
+    console: str = request.form["console"]
+    
+    jogo: Jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+
     return render_template("lista.html", titulo="Jogos", jogos=lista)
 
 app.run(debug=True)
